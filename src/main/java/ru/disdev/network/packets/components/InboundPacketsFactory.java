9519ece67cbfe405462658d52db3151ffd1744d6
d1slike <eldustru@yandex.ru>
2016-07-27 11:49:26 +0300

    New client packets(AUTH_REQUEST, CREATE_GROUP_REQUEST, GCM_REGISTRATION_REQUEST)

--- src/main/java/ru/disdev/network/packets/components/InboundPacketsFactory.java
+++ src/main/java/ru/disdev/network/packets/components/InboundPacketsFactory.java
@@ -1,7 +1,9 @@
 package ru.disdev.network.packets.components;
 
 import ru.disdev.network.packets.ClientPacket;
-import ru.disdev.network.packets.in.RegistrationDeviceRequest;
+import ru.disdev.network.packets.in.AuthRequest;
+import ru.disdev.network.packets.in.CreateGroupRequest;
+import ru.disdev.network.packets.in.GCMRegistartionRequest;
 
 /**
  * Created by Dislike on 18.07.2016.
@@ -9,8 +11,12 @@ import ru.disdev.network.packets.in.RegistrationDeviceRequest;
 public class InboundPacketsFactory {
     public static ClientPacket create(final byte key) {
         switch (key) {
-            case InboundPacketsKeys.RegistrationDeviceRequest:
-                return new RegistrationDeviceRequest();
+            case InboundPacketsKeys.AUTH_REQUEST:
+                return new AuthRequest();
+            case InboundPacketsKeys.GCM_REGISTRATION_REQUEST:
+                return new GCMRegistartionRequest();
+            case InboundPacketsKeys.CREATE_GROUP_REQUEST:
+                return new CreateGroupRequest();
         }
         return null;
     }
--- src/main/java/ru/disdev/network/packets/components/InboundPacketsKeys.java
+++ src/main/java/ru/disdev/network/packets/components/InboundPacketsKeys.java
@@ -4,5 +4,7 @@ package ru.disdev.network.packets.components;
  * Created by Dislike on 18.07.2016.
  */
 public class InboundPacketsKeys {
-    public static final byte RegistrationDeviceRequest = 11;
+    public static final byte AUTH_REQUEST = 1;
+    public static final byte GCM_REGISTRATION_REQUEST = 2;
+    public static final byte CREATE_GROUP_REQUEST = 3;
 }
